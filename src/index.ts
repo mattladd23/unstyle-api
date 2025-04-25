92672f78b03092708875bb40160bffe0ed367114
Matt <matt-jbl@hotmail.co.uk>
2025-04-25 14:38:18 +0100

    A loada project setup

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,21 @@ import express from 'express';
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const session = require("express-session");
+const dummyText = 'Howay the lasses';
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: {
+      expires: 600000
+  }
+}));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Unstyle.io');
+  res.send(`Welcome to Unstyle.io ${dummyText}`);
 });
 
 app.listen(port, () => {
