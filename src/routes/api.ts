48edda7e2f6ac5a78bfb40bef69c4377592288ec
Matt <matt-jbl@hotmail.co.uk>
2025-04-28 11:16:22 +0100

    Grabbed all property selectors - I think

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,8 +2,15 @@ import { launchBrowser } from "../utils/launchBrowser";
 
 export const callAPI = async () => {
     const { browser, page } = await launchBrowser();
-    await page.goto("https://www.bbc.co.uk/");
-    await page.screenshot({ path: "src/data/temp/test-screenshot.png" });
+    await page.goto("https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/");
+    // await page.screenshot({ path: "src/data/temp/test-screenshot.png" });
+
+    const selectorsList = ".sidebar-body > ol > li:nth-of-type(8) > details > ol > li";
+
+    await page.waitForSelector(selectorsList);
+
+    const sidebar = await page.$$eval(selectorsList, el => el.length);
+    console.log(sidebar);
 
     await browser.close();
 } 
