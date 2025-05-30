0dd988851ecb3297c33088fc75d92bab64cf8bdc
Matt <matt-jbl@hotmail.co.uk>
2025-05-30 18:42:31 +0100

    Removed commented out code

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -22,39 +22,5 @@ export const callAPI = async () => {
 
     await visitUrls(urls, page, humanBehaviorSimulator);
 
-    // console.log(urls);
-
-    // let numUrlsVisited: number = 0;
-    // let notEmptyInitialValues: number = 0;
-
-    // for (const url of urls) {
-    //     if (numUrlsVisited >= urls.length) { return; }
-    //     await page.goto(url);
-    //     numUrlsVisited++;
-    //     console.log(`Navigating to: ${url}`);
-    //     await humanBehaviorSimulator.simulateRandomBehavior();
-    //     const initialValue = await page.$$eval(".properties tr:first-child", (values) => {
-    //         if (!values[0]) {
-    //             return { value: "No values table found", foundInitialValue: false };
-    //         }
-    //         if (values[0].querySelector('a')?.textContent?.includes("Initial value")) {
-    //             return { value: values[0].querySelector('code')?.textContent?.trim(), foundInitialValue: true };
-    //         } else {
-    //             return { value: "No initial value found", foundInitialValue: false };
-    //         }
-    //     });
-
-    //     if (initialValue.foundInitialValue) {
-    //         notEmptyInitialValues++;
-    //     }
-
-    //     console.log({
-    //         "url": url,
-    //         "initialValue": initialValue
-    //     });
-        
-    //     console.log(`Visited ${numUrlsVisited} URLs, found ${notEmptyInitialValues} with initial values.`);
-    // }
-
     await browser.close();
 }
