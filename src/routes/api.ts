0fa9adbfaa955110a7c14685864946b1eaba81c0
Matt <matt-jbl@hotmail.co.uk>
2025-06-02 13:58:45 +0100

    We have a working db!

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,9 @@ import { startPage } from "../data/pages/startPage.js";
 import { getUrls } from "../utils/getUrls.js";
 import { HumanBehaviorSimulator } from "../utils/HumanBehaviourSimulator.js";
 import { visitUrls } from "../utils/visitUrls.js";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import { InitialValuesData } from "../types/InitialValuesData.js";
 
 export const callAPI = async () => {
     const { browser, page } = await launchBrowser();
@@ -22,7 +25,18 @@ export const callAPI = async () => {
 
     const initialValuesObj = await visitUrls(urls, page, humanBehaviorSimulator);
 
-    console.log("Initial values object:", JSON.stringify(initialValuesObj));
+    const file = 'src/data/db.json';
+    const adapter = new JSONFile<InitialValuesData>(file);
+    const db = new Low<InitialValuesData>(adapter, { initialValues: [] });
+
+    await db.read();
+
+    // Wipe existing data
+    db.data = { initialValues: [] }
+
+    db.data.initialValues.push(...initialValuesObj);
+
+    await db.write();
 
     await browser.close();
 }
