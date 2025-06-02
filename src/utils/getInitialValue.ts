f21fed727fdb20ad79ffd44353a7c775ea49ef40
Matt <matt-jbl@hotmail.co.uk>
2025-06-02 09:36:40 +0100

    Made return objects easier to read

--- src/utils/getInitialValue.ts
+++ src/utils/getInitialValue.ts
@@ -3,13 +3,28 @@ import { Page } from 'puppeteer';
 export const getInitialValue = async (page: Page, url: string) => {
     const initialValueResult = await page.$$eval(".properties tr:first-child", (rows, urlFromNode) => {
         if (!rows[0]) {
-            return { value: "No values table found", foundInitialValue: false, url: urlFromNode, valuesFound: 0 };
+            return {
+                value: "No values table found",
+                foundInitialValue: false,
+                url: urlFromNode,
+                valuesFound: 0
+            };
         }
         if (rows[0].querySelector('a')?.textContent?.includes("Initial value")) {
             const initialValueCount = rows[0].querySelectorAll('code');
-            return { value: initialValueCount[0].textContent?.trim(), foundInitialValue: true, url: urlFromNode, valuesFound: initialValueCount.length };
+            return {
+                value: initialValueCount[0].textContent?.trim(),
+                foundInitialValue: true,
+                url: urlFromNode,
+                valuesFound: initialValueCount.length
+            };
         } else {
-            return { value: "No initial value found", foundInitialValue: false, url: urlFromNode, valuesFound: 0 };
+            return {
+                value: "No initial value found",
+                foundInitialValue: false,
+                url: urlFromNode,
+                valuesFound: 0
+            };
         }
     },
         url
